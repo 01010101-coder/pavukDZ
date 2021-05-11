f938d645342e6b8b538d7c8aca01d5e6a2ac6c7b
vladyahnovets <v.yahnavets@gmail.com>
2021-05-11 18:44:58 +0300

    шрш

--- functions.py
+++ functions.py
@@ -12,14 +12,14 @@ def parse_table(table):
 
     name_tr = table.find('div',{'class': 'product-card__title'})
     # получаю текст вопроса
-    name = name_tr.find('div').text.replace('<br />', '\n').strip()
+    name = name_tr.find('div').text.strip()
 
-    ingridients_tr = name_tr.find_all('div', {'class': 'product-card__description'})
+    ingridients_tr = name_tr.find('div', {'class': 'product-card__description'})
     ingridients = ingridients_tr.text.strip()
 
     # ответы
     cena_tr = table.find('p', {'class': 'product-card__modification-info-price'})
-    cena = cena_tr.find_all('#text')[0].find('div').text.replace('<br />', '\n').strip()
+    cena = cena_tr.find_all('#text')[0].find('div').text.strip()
 
     res = res.append(pd.DataFrame([
         [name, ingridients, cena]
